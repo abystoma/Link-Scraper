9bfba04832ac3ce7a3ad9e52d888c65ae7c3cd09
Devon Reid <fireborn1003@gmail.com>
2020-11-01 17:45:41 +0600

    FEAT: Conversion to a library function

--- index.js
+++ index.js
@@ -1,47 +1,37 @@
-//const [,,directory,url,file] = process.argv
-const directory = "dir",
-  url = "https://docs.python.org/3/",
-  file = "index.html";
-const fs = require("fs");
-const color = require("colors");
-const axios = require("axios");
-const jsdom = require("jsdom");
+const fs = require('fs');
+const color = require('colors');
+const axios = require('axios');
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-try {
-  fs.mkdirSync(__dirname + "/" + directory);
-} catch (err) {
-  console.log(err.message);
-}
+module.exports = function (directory, url, file) {
+	try {
+		fs.mkdirSync(__dirname + '/' + directory);
+	} catch (err) {
+		console.log(err.message);
+	}
 
-axios.get(url).then((data) => {
-  fs.writeFileSync(__dirname + "/" + directory + "/" + file, data.data);
-  const {
-    window: { document },
-  } = new JSDOM(data.data);
-  const links = document.querySelectorAll("a");
-  //console.log(links);
-  const arr = ["a", "title", "ul", "ol", "li", "p"];
-  arr.forEach((tag, index) => {
-    const elems = document.querySelectorAll(tag);
-    console.log(index, tag);
-  });
-  let content = `<head> 
-    <style> 
-        a{
-            color: #111;
-            padding: 5px;
-            display: block;
-            font-family: Arial;
-        }
-    </style>
-  </head>`;
-  Array.from(links).forEach((a) => {
-    const isAbsolute = a.href.startsWith("https://");
-    console.log(color.blue.bold(a.textContent));
-    content += `<a href = ${
-      isAbsolute ? a.href : "https://docs.python.org/" + a.href
-    }> ${a.textContent} </a> \n </br>`;
-  });
-  fs.writeFileSync(__dirname + "/" + directory + "/" + "link.html", content);
-});
+	axios.get(url).then((data) => {
+		fs.writeFileSync(__dirname + '/' + directory + '/' + file, data.data);
+		const { window: { document } } = new JSDOM(data.data);
+		const links = document.querySelectorAll('a');
+		let content = `<head> 
+      <style> 
+          a{
+              color: #111;
+              padding: 5px;
+              display: block;
+              font-family: Arial;
+          }
+      </style>
+    </head>`;
+		Array.from(links).forEach((a) => {
+			const isAbsolute = a.href.startsWith('https://');
+			console.log(color.blue.bold(a.textContent));
+			content += `<a href = ${isAbsolute
+				? a.href
+				: 'https://docs.python.org/' + a.href}> ${a.textContent} </a> \n </br>`;
+		});
+		fs.writeFileSync(__dirname + '/' + directory + '/' + 'link.html', content);
+	});
+};
